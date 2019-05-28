c82bc7860bb6549cbfd878b75593a50f71689f38
lidor115 <lidor115@gmail.com>
2019-05-28 20:40:45 +0300

    fake controller

---
+++ REST-WEB/Controllers/MyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace REST_WEB.Controllers
+{
+    public class MyController : Controller
+    {
+            [HttpGet]
+            public ActionResult display(string nameFile, int time)
+            {
+                ViewBag.lon = 50;
+                ViewBag.lat = 50;
+                return View();
+            }
+            // GET: save
+            [HttpGet]
+            public ActionResult save(string ip1, string ip2, string ip3, string ip4, int port, int time, int duration, string nameFile)
+            {
+                string ip = ip1 + "." + ip2 + "." + ip3 + "." + ip4;
+                return View();
+            }
+        }
+    }
